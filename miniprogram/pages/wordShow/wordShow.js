e6b6b1f20c56428288f7e1f68105384231469e78
ChenTingRu99 <996629961@qq.com>
2020-05-09 16:45:17 +0800

    ctr
    1、将单词页面之间用滑动相连接
    2、将原先的用户评级按钮（仍需记忆、下一个、太简单）的点击加上样式变化

--- miniprogram/pages/wordShow/wordShow.js
+++ miniprogram/pages/wordShow/wordShow.js
@@ -1,5 +1,8 @@
 //setdata使用数据通道消耗资源，为避免对不在ml文件中显示的数据不断设置，将本地属性word中仅存放id2,其余存在在data以外的变量wordOutData中
 var app = getApp()
+var startX, endX;
+var moveFlag = true;
+
 const innerAudioContext = wx.createInnerAudioContext();
 Page({
 
@@ -9,83 +12,40 @@ Page({
   timeBegin: "",
   timeEnd: "",
   data: {
-    word: {},  
+    word: {},
     id: 1,
-    
+    ani: '',
+    list: ['太简单', '下一个', '仍需记忆']
+  },
+
+  click: function(e) {
+    var index = e.currentTarget.dataset.index;
+    app.globalData.overallWordList[index].theDifficultyByUser = index;
+    this.setData({
+      selectedindex: index
+    });
   },
 
 
-  goindex() {
-    wx.navigateTo({
-      url: '../wordTest/wordTest',
-    })
-  },
-  timecalculate: function (e) {
+  timecalculate: function(e) {
     var that = this
     var timestamp = Date.parse(new Date());
-    
+
     var timeResult = app.getTimeforUse(timestamp);
-    
-    that.timeEnd=timeResult
+
+    that.timeEnd = timeResult
     console.log("timeEnd", that.timeEnd)
     console.log("timebegin", that.timeBegin)
     var b = ((that.timeEnd - that.timeBegin) > 60) ? 60 : (that.timeEnd - that.timeBegin)
-   console.log("b",b)
-    that.timeBegin=timeResult
-    
-    that.data.word.forEach(function (item, index) {
-      app.globalData.overallWordList[index].learnTime=b
+    console.log("b", b)
+    that.timeBegin = timeResult
+
+    that.data.word.forEach(function(item, index) {
+      app.globalData.overallWordList[index].learnTime = b
     })
-   
-    
 
-  },
 
-  goTest0(e) {
-    var index = e.currentTarget.dataset.index;
-    app.globalData.overallWordList[index].theDifficultyByUser=0
-    var idm = this.data.id;
-    if (idm < app.globalData.overallWordList.length) {
-      console.log(this.data.id);
-      this.setData({
-        id: idm + 1
-      })
-    } else {
-      wx.navigateTo({
-        url: '../wordTest/wordTest',
-      })
-    }
-  },
 
-  goTest1(e) {
-    var index = e.currentTarget.dataset.index;
-    app.globalData.overallWordList[index].theDifficultyByUser=1
-    var idm = this.data.id;
-    if (idm < app.globalData.overallWordList.length) {
-      console.log(this.data.id);
-      this.setData({
-        id: idm + 1
-      })
-    } else {
-      wx.navigateTo({
-        url: '../wordTest/wordTest',
-      })
-    }
-  },
-  goTest2(e) {
-    var index = e.currentTarget.dataset.index;
-    app.globalData.overallWordList[index].theDifficultyByUser=2
-    var idm = this.data.id;
-    if (idm < app.globalData.overallWordList.length) {
-      console.log(this.data.id);
-      this.setData({
-        id: idm + 1
-      })
-    } else {
-      wx.navigateTo({
-        url: '../wordTest/wordTest',
-      })
-    }
   },
 
   sound_AE(e) {
@@ -93,10 +53,10 @@ Page({
     var index = e.currentTarget.dataset.index;
     ++this.data.word[index].numclick;
     innerAudioContext.src = "cloud://cloud-14ij5.636c-cloud-14ij5-1301705689/" + this.data.word[index].word + "_AE.mp3";
-    
-  setTimeout(function () {
-    innerAudioContext.play();
-  }.bind(this), 300)
+
+    setTimeout(function() {
+      innerAudioContext.play();
+    }.bind(this), 300)
 
   },
   sound_BE(e) {
@@ -104,53 +64,53 @@ Page({
     var index = e.currentTarget.dataset.index;
     ++this.data.word[index].numclick;
     innerAudioContext.src = "cloud://cloud-14ij5.636c-cloud-14ij5-1301705689/" + this.data.word[index].word + "_BE.mp3";
-    
-  setTimeout(function () {
-    innerAudioContext.play();
-  }.bind(this), 300)
+
+    setTimeout(function() {
+      innerAudioContext.play();
+    }.bind(this), 300)
 
   },
 
   addPropertyInListLocally(a) {
     var word = a
     var that = this
-    
-   
-    if (app.globalData.overallWordList[0].theDifficultyByUser == undefined) {
-      that.data.word.forEach(function (item, index) {
 
-        app.globalData.overallWordList[index].theDifficultyByUser=-1
+
+    if (app.globalData.overallWordList[0].theDifficultyByUser == undefined) {
+      that.data.word.forEach(function(item, index) {
+
+        app.globalData.overallWordList[index].theDifficultyByUser = -1
 
       })
-      
+
     }
-    if (that.data.word[0].id2  == undefined) {
-      that.data.word.forEach(function (item, index) {
+    if (that.data.word[0].id2 == undefined) {
+      that.data.word.forEach(function(item, index) {
 
         var id2 = "word[" + index + "].id2";
         that.setData({
-          [id2]: index+1
+          [id2]: index + 1
         })
 
-        
-     })
- 
-   
-    
+
+      })
+
+
+
     }
     if (app.globalData.overallWordList[0].numclick == undefined) {
-      that.data.word.forEach(function (item, index) {
+      that.data.word.forEach(function(item, index) {
 
-        app.globalData.overallWordList[index].numclick=0
- 
-     })
-    
+        app.globalData.overallWordList[index].numclick = 0
+
+      })
+
     }
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.setData({
       word: app.globalData.overallWordList,
     })
@@ -159,64 +119,203 @@ Page({
       var timestamp = Date.parse(new Date());
       console.log("当前时间戳为：" + timestamp);
       var timeResult = app.getTimeforUse(timestamp);
-     
-       this.timeBegin=timeResult
-      
+
+      this.timeBegin = timeResult
+
 
     }
   },
 
+
+
+  touchStart: function(e) {
+
+    startX = e.touches[0].pageX; // 获取触摸时的原点
+
+    moveFlag = true;
+
+  },
+
+  // 触摸移动事件
+
+  touchMove: function(e) {
+
+    endX = e.touches[0].pageX; // 获取触摸时的原点
+
+    if (moveFlag) {
+
+      if (endX - startX > 50) {
+
+        console.log("move right");
+
+        this.move2right();
+
+        moveFlag = false;
+
+      }
+
+      if (startX - endX > 50) {
+
+        console.log("move left");
+
+        this.move2left();
+
+        moveFlag = false;
+
+      }
+
+    }
+
+  },
+
+  // 触摸结束事件
+
+  touchEnd: function(e) {
+
+    moveFlag = true; // 回复滑动事件
+
+  },
+
+  //向左滑动操作
+
+  move2left() {
+
+    var that = this;
+
+    if (this.data.id == app.globalData.overallWordList.length) {
+
+      return
+
+    }
+
+    var animation = wx.createAnimation({
+
+      duration: 1000,
+
+      timingFunction: 'ease',
+
+      delay: 100
+
+    });
+
+    animation.opacity(0.2).translate(-500, 0).step()
+
+    this.setData({
+
+      ani: animation.export(),
+      selectedindex: -1
+    })
+
+    var idm = that.data.id;
+    if (idm < app.globalData.overallWordList.length) {
+      console.log(that.data.id);
+      that.setData({
+        id: idm + 1
+      })
+    } else {
+      wx.navigateTo({
+        url: '../wordTest/wordTest',
+      })
+    }
+
+  },
+
+  //向右滑动操作
+
+  move2right() {
+
+    var that = this;
+
+    if (this.data.id == 1) {
+
+      return
+
+    }
+
+    var animation = wx.createAnimation({
+
+      duration: 1000,
+
+      timingFunction: 'ease',
+
+      delay: 100
+
+    });
+
+    animation.opacity(0.2).translate(500, 0).step()
+
+    this.setData({
+
+      ani: animation.export(),
+      selectedindex: -1
+    })
+
+    var idm = that.data.id;
+    if (idm > 1) {
+      console.log(that.data.id);
+      that.setData({
+        id: idm - 1
+      })
+    }
+
+  },
+
+
+
+
+
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-   
-    
-   
+  onReady: function() {
+
+
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-   
+  onShow: function() {
 
-    
+
+
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
-})                    
+})
