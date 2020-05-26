fccb7edc2b49823d66b4f221fce2388b8f1cb605
Tomoyo <414185759@qq.com>
2020-05-26 20:36:06 +0800

    刘承轩 新增自律遗忘函数
    Signed-off-by: Tomoyo <414185759@qq.com>

---
+++ cloudfunctions/fgt_cet6/index.js
@@ -0,0 +1,25 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+  env: 'cloud-14ij5'
+})
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  let myword = event._word
+  let userx = event._userx
+  let m_n = event._memory_num
+
+  var memory_num = {}
+  memory_num[userx] = m_n
+  return await db.collection('cet6').where({
+    word: myword
+  })
+    .update({
+      data: {
+        memory_num
+      }
+    })
+
+}
