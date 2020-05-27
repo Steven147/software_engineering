6cb47f5606559db43fd662bce3488dbd10571a8f
faderer <245128111@qq.com>
2020-05-27 22:11:55 +0800

    完成单词的分类

--- miniprogram/pages/infomationGet/infomationGet.js
+++ miniprogram/pages/infomationGet/infomationGet.js
@@ -115,23 +115,16 @@ Page({
     console.log(result[i])
 }*/
  
-    app.yun1()
-    app.yun2()
-    app.yun3()
-    app.yun4()
-  
- 
-  // var temp1 = []
-  // temp1 = app.globalData.wordfetch[0].cet6
-  // console.log(temp1)
-  // console.log("字典元素按value值排序: ");
-  // var res2 = Object.keys(temp1).sort(function(a,b){ return temp1[a]["score"]-temp1[b]["score"];});
-  // for(var key in res2){
-  //   app.yun1(res2[key])
-  //     console.log("key: " + res2[key] + " ,value: " + temp1[res2[key]]);
-  // }
-  // app.globalData.rememberList = app.globalData.rememberList.contact(temp1.splice(0,this.data.numOfReview))
-  // console.log('app.globalData.rememberList',app.globalData.rememberList)
+  app.yun1()
+  app.yun2()
+  app.yun3()
+  app.yun4()
+
+  var temp1 = []
+  temp1 = app.globalData.wordfetch[0].cet6
+  console.log(temp1)
+  app.globalData.rememberList = app.globalData.rememberList.contact(temp1.splice(0,this.data.numOfReview))
+  console.log('app.globalData.rememberList',app.globalData.rememberList)
 
   wx.hideLoading();
   this.setData({
